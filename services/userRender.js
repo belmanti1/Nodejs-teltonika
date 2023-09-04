c7f807b883578943c4102ac34cedd7716df23ec4
Brahim El Manti <81900171+belmanti1@users.noreply.github.com>
2023-09-04 12:41:48 +0100

    Update userRender.js

--- services/userRender.js
+++ services/userRender.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 exports.lister_user=(req,res)=>{
      //make a get request to /api/groupes
-     axios.get('http://localhost:3006/user/api/users')
+     axios.get('https://gep-iot-jsmw.onrender.com/user/api/users')
      .then(function(response){
          console.log(response)
          res.render('lister_user',{users:response.data,name: "administrateur"})
@@ -14,7 +14,7 @@ exports.add_user=(req,res)=>{
     res.render('add_user',{users:"new Data",name: "administrateur"});
 }
 exports.update_user=(req,res)=>{
-    axios.get('http://localhost:3006/user/api/users',{ params : {id:req.query.id }})
+    axios.get('https://gep-iot-jsmw.onrender.com/user/api/users',{ params : {id:req.query.id }})
     .then(function(userdata){
         res.render("update_user",{user:userdata.data,name: "administrateur"})
     })
