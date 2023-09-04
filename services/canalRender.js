dedaf61f05f097cc522f3bdb6b08edf2de300fff
Brahim El Manti <81900171+belmanti1@users.noreply.github.com>
2023-09-04 12:38:11 +0100

    Update canalRender.js

--- services/canalRender.js
+++ services/canalRender.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 exports.lister_canal=(req,res)=>{
     //make a get request to /api/canals
-    axios.get('http://localhost:3006/canal/api/canaux')
+    axios.get('https://gep-iot-jsmw.onrender.com/canal/api/canaux')
         .then(function(response){
             console.log(response)
             res.render('lister_canal',{canaux:response.data,name: req.user.name})
@@ -14,7 +14,7 @@ exports.add_canal=(req,res)=>{
     res.render('add_canal',{canaux:"new Data",name: req.user.name});
 }
 exports.update_canal=(req,res)=>{
-    axios.get('http://localhost:3006/canal/api/canaux',{ params : {id:req.query.id }})
+    axios.get('https://gep-iot-jsmw.onrender.com/canal/api/canaux',{ params : {id:req.query.id }})
     .then(function(canaldata){
         res.render("update_canal",{canal:canaldata.data,name: req.user.name})
     })
@@ -25,7 +25,7 @@ exports.update_canal=(req,res)=>{
 
 exports.Adminlister_canal=(req,res)=>{
     //make a get request to /api/canals
-    axios.get('http://localhost:3006/canal/api/canaux')
+    axios.get('https://gep-iot-jsmw.onrender.com/canal/api/canaux')
         .then(function(response){
             console.log(response)
             res.render('admin/lister_canal',{canaux:response.data,name: "administrateur"})
@@ -38,7 +38,7 @@ exports.Adminadd_canal=(req,res)=>{
     res.render('admin/add_canal',{canaux:"new Data",name: "administrateur"});
 }
 exports.Adminupdate_canal=(req,res)=>{
-    axios.get('http://localhost:3006/canal/api/canaux',{ params : {id:req.query.id }})
+    axios.get('https://gep-iot-jsmw.onrender.com/canal/api/canaux',{ params : {id:req.query.id }})
     .then(function(canaldata){
         res.render("admin/update_canal",{canal:canaldata.data,name: "administrateur"})
     })
