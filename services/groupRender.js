704aaf9d90466dbae74f2ff7d3109c02b3e86f13
Brahim El Manti <81900171+belmanti1@users.noreply.github.com>
2023-09-04 13:07:46 +0100

    Update groupRender.js

--- services/groupRender.js
+++ services/groupRender.js
@@ -3,7 +3,7 @@ const Canal=require('../models/canalModel');
 
 exports.lister_group=(req,res)=>{
     //make a get request to /api/groupes
-    axios.get('http://localhost:3006/group/api/groupes')
+    axios.get('https://gep-iot-jsmw.onrender.com/group/api/groupes')
         .then(function(response){
             console.log(response)
             res.render('lister_group',{groupes:response.data,name: req.user.name})
